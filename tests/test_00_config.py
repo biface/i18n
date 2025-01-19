24b017b90bbbbb3064143c6ce492cb306d4870d2
biface <fabrice.moutte.wn@gmail.com>
2025-01-19 18:43:52 +0100

    Build:Updating tests files.

--- tests/test_00_config.py
+++ tests/test_00_config.py
@@ -4,7 +4,9 @@ import uuid
 from pathlib import Path
 
 import pytest
+from unittest.mock import patch
 from email_validator import EmailNotValidError
+from datetime import datetime, timedelta
 
 from i18n_tools.config import Config
 
@@ -28,7 +30,11 @@ def temp_dir_with_locales():
         setup:
           paths:
             application:
-              - app/locales
+                base: ''
+                modules:
+                  - mod1/locales
+                  - mod2/pkg1/locales
+                  - mod2/pkg2/locales
           languages:
             source: en
             hierarchy:
@@ -58,7 +64,11 @@ def temp_dir_with_locales():
         setup:
           paths:
             application:
-              - app/locales
+                base: ''
+                modules:
+                    - mod1/locales
+                    - mod2/pkg1/locales
+                    - mod2/pkg2/locales
           language:
             source: en
             hierarchy:
@@ -83,8 +93,9 @@ def temp_dir_with_locales():
         """
     )
 
-    return list((temp_dir, locales_dir, temp_path, config_yaml, config_toml, config_err_yaml))
-
+    return list(
+        (temp_dir, locales_dir, temp_path, config_yaml, config_toml, config_err_yaml)
+    )
 
 temp_data = temp_dir_with_locales()
 config = Config(temp_data[3])
@@ -118,7 +129,10 @@ def test_config_with_temp_file():
     assert config.setup[["domains", "package"]] == ["domain1"]
     assert config.setup[["domains", "application"]] == ["domain2", "domain3"]
     assert config.get(["details", "name"]) == "Configuration test file"
-    assert config.get(["authors", "123e4567-e89b-12d3-a456-426614174000", "first_name"]) == "John"
+    assert (
+        config.get(["authors", "123e4567-e89b-12d3-a456-426614174000", "first_name"])
+        == "John"
+    )
     with pytest.raises(KeyError):
         config.get(["detail", "name"])
 
@@ -148,11 +162,17 @@ def test_config_save_with_temp_file(tmp_path):
 
 
 def test_config_set_with_key_controls():
-    config.set("details", {"name": "A configuration for life", "description": "This or nothing"})
+    config.set(
+        "details",
+        {"name": "A configuration for life", "description": "This or nothing"},
+    )
     assert config.get(["details", "name"]) == "A configuration for life"
     assert config.get(["details", "description"]) == "This or nothing"
     with pytest.raises(KeyError):
-        config.set("details", {"name": "A configuration for life", "descriptions": "This or nothing"})
+        config.set(
+            "details",
+            {"name": "A configuration for life", "descriptions": "This or nothing"},
+        )
 
 
 def test_config_unknown_file():
@@ -162,18 +182,21 @@ def test_config_unknown_file():
     config.set(["setup", "paths", "config"], temp_data[3])
 
 
-@pytest.mark.parametrize("args, expected_exception",
+@pytest.mark.parametrize(
+    "args, expected_exception",
     [
         (("", {}), ValueError),
-        (((1,2), {}), ValueError),
+        (((1, 2), {}), ValueError),
         (([], "Path must be specified"), ValueError),
-        (({'key':1}, {}), ValueError),
+        (({"key": 1}, {}), ValueError),
         (("unknown", {}), KeyError),
         (("details", "string"), TypeError),
-    ])
+    ],
+)
 def test_config_set_errors(args, expected_exception):
     with pytest.raises(expected_exception):
         config.set(*args)
+
 
 def test_config_set_errors_special():
     with pytest.raises(TypeError):
@@ -182,58 +205,64 @@ def test_config_set_errors_special():
 
 
 def test_add_author():
-    config.add_author("Albert",
-                      "Dupont",
-                      "albert.dupont@local.net",
-                      "",
-                      ["en", "fr"])
-    assert config.get_author("123e4567-e89b-12d3-a456-426614174000")["email"] == "john.doe@example.com"
+    config.add_author("Albert", "Dupont", "albert.dupont@local.net", "", ["en", "fr"])
+    assert (
+        config.get_author("123e4567-e89b-12d3-a456-426614174000")["email"]
+        == "john.doe@example.com"
+    )
+
 
 def test_add_author_email_already_exists():
     with pytest.raises(KeyError):
-        config.add_author("Albert",
-                          "Dupond",
-                          "albert.dupont@local.net",
-                          "",
-                          ["en", "fr"])
+        config.add_author(
+            "Albert", "Dupond", "albert.dupont@local.net", "", ["en", "fr"]
+        )
+
 
 def test_add_author_email_error():
     with pytest.raises(EmailNotValidError):
-        config.add_author("Albert",
-                          "Durand",
-                          "albert@host",
-                          "",
-                          ["en", "fr"])
+        config.add_author("Albert", "Durand", "albert@host", "", ["en", "fr"])
+
 
 def test_get_author_by_email():
-    assert config.get_author("albert.dupont@local.net")["email"] == "albert.dupont@local.net"
+    assert (
+        config.get_author("albert.dupont@local.net")["email"]
+        == "albert.dupont@local.net"
+    )
+
 
 def test_get_autor_error_false_uuid():
     with pytest.raises(KeyError):
         config.get_author(str(uuid.uuid4()))
 
+
 def test_get_autor_email_error():
     with pytest.raises(ValueError):
         config.get_author("albert@host")
 
+
 def test_remove_author():
     assert config.remove_author("albert.dupont@local.net") == True
-    config.add_author("Albert",
-                      "Dupont",
-                      "albert.dupont@local.net",
-                      "",
-                      ["en", "fr"])
+    config.add_author("Albert", "Dupont", "albert.dupont@local.net", "", ["en", "fr"])
     id = config._email_index["albert.dupont@local.net"]
     assert config.remove_author(id) == True
 
-@pytest.mark.parametrize("author, flag", [
-    ("lbert.dupont@local.net", False),
-    ("albert.dupont@local.net", False),
-    (str(uuid.uuid4()), False),
-])
+
+@pytest.mark.parametrize(
+    "author, flag",
+    [
+        ("lbert.dupont@local.net", False),
+        ("albert.dupont@local.net", False),
+        (str(uuid.uuid4()), False),
+    ],
+)
 def test_remove_author_unsuccessful(author, flag):
     assert config.remove_author(author) == flag
+
 
 def test_remove_author_error():
     with pytest.raises(ValueError):
         config.remove_author("albert@host")
+
+
+
--- tests/test_00_locale.py
+++ tests/test_00_locale.py
@@ -1,65 +1,85 @@
 import pytest
-from i18n_tools.locale import normalize_language_tag, is_valid_language_tag, validate_and_normalize_language_tags
+from i18n_tools.locale import (
+    normalize_language_tag,
+    is_valid_language_tag,
+    validate_and_normalize_language_tags,
+)
 
 
-@pytest.mark.parametrize("tag, result", [
-    ("123", False),
-    ("English", False),
-    ("a", False,),
-    ("de-1996", True),
-    ("en", True),
-    ("en-Latn-US", True),
-    ("en-US", True),
-    ("en-US-!", False),
-    ("en-x-", False),
-    ("en-x-private", True),
-    ("en-x-private-",False),
-    ("eng-Latin", False),
-    ("es-419", True),
-    ("fr", True),
-    ("fr+FR", False),
-    ("fr-CA", True),
-    ("fr-FR", True),
-    ("fr-fr", True),
-    ("francais", False),
-    ("frç", False),
-    ("ja-Latn-hepburn-heploc", True),
-    ("ja.Latn/hepburn@heploc", False),
-    ("sl-rozaj-biske-1994", True),
-    ("zh-Hans-1234", False),
-    ("zh-Hans-CN", True),
-    ("zh-Hant", True),
-    ("zh-Latn-pinyin", True)
-])
+@pytest.mark.parametrize(
+    "tag, result",
+    [
+        ("123", False),
+        ("English", False),
+        (
+            "a",
+            False,
+        ),
+        ("de-1996", True),
+        ("en", True),
+        ("en-Latn-US", True),
+        ("en-US", True),
+        ("en-US-!", False),
+        ("en-x-", False),
+        ("en-x-private", True),
+        ("en-x-private-", False),
+        ("eng-Latin", False),
+        ("es-419", True),
+        ("fr", True),
+        ("fr+FR", False),
+        ("fr-CA", True),
+        ("fr-FR", True),
+        ("fr-fr", True),
+        ("francais", False),
+        ("frç", False),
+        ("ja-Latn-hepburn-heploc", True),
+        ("ja.Latn/hepburn@heploc", False),
+        ("sl-rozaj-biske-1994", True),
+        ("zh-Hans-1234", False),
+        ("zh-Hans-CN", True),
+        ("zh-Hant", True),
+        ("zh-Latn-pinyin", True),
+    ],
+)
 def test_is_valid_language_tag(tag, result):
     assert is_valid_language_tag(tag) == result
 
 
-@pytest.mark.parametrize("tag, result", [
-    ("En", "en"),
-    ("FR", "fr"),
-    ("fr-fr", "fr-FR"),
-    ("FR-ca", "fr-CA"),
-    ("EN-IE", "en-IE"),
-    ("ja-latn-Hepburn-Heploc", "ja-Latn-hepburn-heploc"),
-])
+@pytest.mark.parametrize(
+    "tag, result",
+    [
+        ("En", "en"),
+        ("FR", "fr"),
+        ("fr-fr", "fr-FR"),
+        ("FR-ca", "fr-CA"),
+        ("EN-IE", "en-IE"),
+        ("ja-latn-Hepburn-Heploc", "ja-Latn-hepburn-heploc"),
+    ],
+)
 def test_normalize_language_tag(tag, result):
     assert normalize_language_tag(tag) == result
+
 
 def test_is_valid_language_tag_invalid():
     with pytest.raises(ValueError):
         normalize_language_tag("ja.Latn/hepburn@heploc")
 
 
-@pytest.mark.parametrize("tags, expected", [
-    (["en", "FR", "es-MX", "zh-Hans", "de-AT"], ["en", "fr", "es-MX", "zh-Hans", "de-AT"]), #Valid
-    (["EN", "fr", "ES-mx"], ["en", "fr", "es-MX"]),  # Casse
-    (["pt-BR", "ja", "ko"], ["pt-BR", "ja", "ko"]),  # Well formed
-
-])
+@pytest.mark.parametrize(
+    "tags, expected",
+    [
+        (
+            ["en", "FR", "es-MX", "zh-Hans", "de-AT"],
+            ["en", "fr", "es-MX", "zh-Hans", "de-AT"],
+        ),  # Valid
+        (["EN", "fr", "ES-mx"], ["en", "fr", "es-MX"]),  # Casse
+        (["pt-BR", "ja", "ko"], ["pt-BR", "ja", "ko"]),  # Well formed
+    ],
+)
 def test_validate_and_normalize_language_tags_valid(tags, expected):
     result = validate_and_normalize_language_tags(tags)
     assert result == expected, f"La normalisation a échoué pour {tags}"
+
 
 @pytest.mark.parametrize(
     "tags, invalid_tag",
