9b8ecf88ac5e174bb9db642d971012844c2ab4ad
biface <fabrice.moutte.wn@gmail.com>
2026-07-03 17:35:49 +0200

    fix: restore models/__init__.py exports (Author/Authors/Translator/Translators)
    - models/__init__.py: merge content from stray models/init.py (correct filename); add module docstring describing the four-level hierarchy; add explicit __all__ so re-exports pass flake8 F401
    - rm models/init.py (mis-named duplicate)

--- src/i18n_tools/models/__init__.py
+++ src/i18n_tools/models/__init__.py
@@ -1,2 +1,17 @@
+from .author import Author, Authors
 from .corpus import Book, Corpus, Encyclopaedia, FallbackBook, Message
 from .repository import Repository
+from .translator import Translator, Translators
+
+__all__ = [
+    "Message",
+    "Book",
+    "Corpus",
+    "FallbackBook",
+    "Encyclopaedia",
+    "Repository",
+    "Author",
+    "Authors",
+    "Translator",
+    "Translators",
+]
--- src/i18n_tools/models/init.py
+++
@@ -1,4 +0,0 @@
-from .author import Author, Authors
-from .corpus import Book, Corpus, Encyclopaedia, FallbackBook, Message
-from .repository import Repository
-from .translator import Translator, Translators
