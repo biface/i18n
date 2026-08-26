76f18b7918af2909806544a407e62124d417dd8f
biface <fabrice.moutte.wn@gmail.com>
2026-08-26 09:31:47 +0200

    chore(release): bump version to 0.9.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ import sys
 project = 'pyi18t-tools'
 copyright = '2025, biface'
 author = 'biface'
-release = '0.9.0rc1'
+release = '0.9.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
